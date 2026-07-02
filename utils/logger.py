fb9e816c304eb16dbe7da6170285a3b4dd98443c
ArtoWare-Indonesia <massubi@gmail.com>
2026-07-02 07:58:55 +0700

    Add logger.py

---
+++ utils/logger.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+
+def setup_logger():
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
+
+    logger = logging.getLogger("NewsScraper")
+    logger.setLevel(logging.INFO)
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    file_handler = logging.FileHandler(
+        log_dir / "scraper.log",
+        encoding="utf-8"
+    )
+
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
