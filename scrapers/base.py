518702ebf98493515025e58e64272600d2a854b5
ArtoWare-Indonesia <massubi@gmail.com>
2026-07-08 15:16:57 -0400

    feat: add abstract BaseScraper class

--- scrapers/base.py
+++ scrapers/base.py
@@ -1,40 +1,17 @@
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 from abc import ABC, abstractmethod
-from config import HEADERS
 
 
 class BaseScraper(ABC):
+    """
+    Base class untuk semua scraper.
+    """
 
-    def __init__(self):
-        self.headers = HEADERS
-
-        self.session = requests.Session()
-
-        retry = Retry(
-            total=3,
-            backoff_factor=1,
-            status_forcelist=[429, 500, 502, 503, 504],
-            allowed_methods=["GET"]
-        )
-
-        adapter = HTTPAdapter(max_retries=retry)
-
-        self.session.mount("http://", adapter)
-        self.session.mount("https://", adapter)
-
-    def fetch(self, url):
-        response = self.session.get(
-            url,
-            headers=self.headers,
-            timeout=15
-        )
-
-        response.raise_for_status()
-
-        return response
+    def __init__(self, source):
+        self.source = source
 
     @abstractmethod
     def scrape(self):
+        """
+        Mengembalikan list of dictionary.
+        """
         pass
